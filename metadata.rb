e7c08a586c9a1212fd7f2f1d7047650fb65af2c5
Corey Hemminger <hemminger@hotmail.com>
2017-11-28 12:20:24 -0600

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,8 @@ license 'Apache-2.0'
 description 'Configures the timezone for node'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.0.5'
-chef_version '>= 12.1' if respond_to?(:chef_version)
+
+chef_version '>= 13.0' if respond_to?(:chef_version)
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
@@ -19,7 +20,7 @@ issues_url 'https://github.com/Stromweld/timezone_iii/issues'
 #
 source_url 'https://github.com/Stromweld/timezone_iii'
 
-%w[amazon centos debian fedora gentoo ubuntu pld redhat suse windows].each do |os|
+%w(amazon centos debian fedora gentoo ubuntu pld redhat suse windows).each do |os|
   supports os
 end
 
