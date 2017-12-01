95eceb238adbdb8ad50a7a97ce2cbc3e5cae54e8
Corey Hemminger <corey.hemminger@nativex.com>
2017-11-30 22:13:10 -0600

    updated version metadata for changelog update

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'hemminger@hotmail.com'
 license 'Apache-2.0'
 description 'Configures the timezone for node'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.1.0'
+version '1.1.1'
 
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
