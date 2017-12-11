b95b53b7bb53cb9d2632ad1eb53937f8aa29db5b
Tomoya Kabe <limit.usus@gmail.com>
2017-12-11 11:21:23 +0900

    Minimum timezone spec

--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -16,3 +16,9 @@ end
 describe port(80), :skip do
   it { should_not be_listening }
 end
+
+describe command('date +%Z') do
+  its('exit_status') { should eq 0 }
+  its('stderr') { should eq '' }
+  its('stdout') { should eq "+03\n" }
+end
