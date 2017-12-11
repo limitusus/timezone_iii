5537e968e52493c2b32cc5ee23358292ab6c6449
Tomoya Kabe <limit.usus@gmail.com>
2017-12-11 16:27:04 +0900

    Test TZ configuration files

--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -22,3 +22,54 @@ describe command('date +%Z') do
   its('stderr') { should eq '' }
   its('stdout') { should eq "+03\n" }
 end
+
+node = JSON.parse(command('ohai').stdout)
+
+is_localtime_symlink = case node['platform_family']
+                       when 'rhel'
+                         node['platform_version'].to_i >= 7
+                       when 'debian'
+                         true
+                       when 'amazon'
+                         false
+                       end
+
+control 'localtime-link' do
+  describe file('/etc/localtime') do
+    it { should be_symlink }
+    its('link_path') { should match /Turkey/ }
+  end
+
+  only_if { is_localtime_symlink }
+end
+
+control 'localtime-file' do
+  describe file('/etc/localtime') do
+    it { should be_file }
+    its('owner') { should eq 'root' }
+    its('group') { should eq 'root' }
+    its('mode') { should eq 0o644 }
+  end
+
+  only_if { !is_localtime_symlink }
+end
+
+describe.one do
+  # RHEL-based platforms
+  describe file('/etc/sysconfig/clock') do
+    its('owner') { should eq 'root' }
+    its('group') { should eq 'root' }
+    its('mode') { should eq 0o644 }
+    content = %{ZONE="Turkey"\n}
+    its('content') { should eq content }
+  end
+
+  # Debian-based platforms
+  describe file('/etc/timezone') do
+    tz = 'Europe/Istanbul'
+    its('owner') { should eq 'root' }
+    its('group') { should eq 'root' }
+    its('mode') { should eq 0o644 }
+    its('content') { should eq "#{tz}\n" }
+  end
+end
